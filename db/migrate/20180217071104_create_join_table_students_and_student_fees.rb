f2808aef38be7048842ca89471699b5e33defb1a
abhichede777 <abhichede777@gmail.com>
2018-02-21 19:34:31 +0530

    Some major changes are done academic year wise batch transfer done new student form newly designed

--- db/migrate/20180217071104_create_join_table_students_and_student_fees.rb
+++ db/migrate/20180217071104_create_join_table_students_and_student_fees.rb
@@ -1,8 +1,18 @@
 class CreateJoinTableStudentsAndStudentFees < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :students, :fee_structures do |t|
-      t.index [:student_id, :fee_structure_id]
-      t.index [:fee_structure_id, :student_id]
+    # create_join_table :students, :fee_structures do |t|
+    #   t.index [:student_id, :fee_structure_id]
+    #   t.index [:fee_structure_id, :student_id]
+    # end
+
+    create_table :fee_structures_students do |t|
+      t.belongs_to :student
+      t.belongs_to :fee_structure
+
+      t.timestamps
     end
+
+    add_index :fee_structures_students, :student_id, name: "student_id"
+    add_index :fee_structures_students, :fee_structure_id, name: "fee_id"
   end
 end
